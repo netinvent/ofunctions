c5608ea57b7866fb17291a3942943bfc3ea541ca
Orsiris de Jong <ozy@netpower.fr>
2021-02-10 00:05:17 +0100

    Fix is_file_older_than to use UTC values

--- ofunctions/file_utils/__init__.py
+++ ofunctions/file_utils/__init__.py
@@ -17,8 +17,8 @@ __intname__ = 'ofunctions.file_utils'
 __author__ = 'Orsiris de Jong'
 __copyright__ = 'Copyright (C) 2017-2021 Orsiris de Jong'
 __licence__ = 'BSD 3 Clause'
-__version__ = '0.7.2'
-__build__ = '2021020901'
+__version__ = '0.7.'
+__build__ = '2021020902'
 
 import json
 import logging
@@ -300,10 +300,12 @@ def file_creation_date(path_to_file: str) -> float:
     Try to get the date that a file was created, falling back to when it was
     last modified if that isn't possible.
     See http://stackoverflow.com/a/39501288/1709587 for explanation.
+
+    Returned epochs are always UTC
     """
     try:
         return os.path.getctime(path_to_file)
-    # Some linxes may not have os.path.getctime ?
+    # Some linuxes may not have os.path.getctime ?
     except AttributeError:
         stat = os.stat(path_to_file)
         try:
@@ -314,14 +316,23 @@ def file_creation_date(path_to_file: str) -> float:
             return stat.st_mtime
 
 
-def is_file_older_than(file: str, years=0, days: int = 0, hours=0, minutes=0, seconds=0) -> bool:
+def is_file_older_than(file: str, years: int = 0, days: int = 0, hours: int = 0,
+                       minutes: int = 0, seconds: int = 0) -> bool:
+    """
+    Simple check if a file is older than given delta from now
+    """
     if not os.path.isfile(file):
         raise FileNotFoundError('[%s] not found.' % file)
     delta = seconds + (minutes * 60) + (hours * 3600) + (days * 86400) + (years * 31536000)
-    return bool((datetime.today().timestamp() - delta - file_creation_date(file)) > 0)
+    return bool((datetime.utcnow().timestamp() - delta - file_creation_date(file)) > 0)
 
 
-def remove_files_older_than(directory: str, years=0, days: int = 0, hours=0, minutes=0, seconds=0) -> None:
+def remove_files_older_than(directory: str, years: int = 0, days: int = 0, hours: int = 0,
+                            minutes: int = 0, seconds: int = 0) -> None:
+    """
+    Remove files older than given delta from now
+    """
+
     if not os.path.isdir(directory):
         raise FileNotFoundError('[%s] not found.' % directory)
 
