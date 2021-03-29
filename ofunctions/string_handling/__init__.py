3b54367ceeeec6e13751622a51771bf8d25f2b5e
Orsiris de Jong <ozy@netpower.fr>
2021-03-29 16:09:14 +0200

    Add more basic string handling functions

--- ofunctions/string_handling/__init__.py
+++ ofunctions/string_handling/__init__.py
@@ -18,8 +18,8 @@ __author__ = 'Orsiris de Jong'
 __copyright__ = 'Copyright (C) 2014-2021 Orsiris de Jong'
 __description__ = 'Simple string sanitization functions'
 __licence__ = 'BSD 3 Clause'
-__version__ = '0.1.1'
-__build__ = '2021020901'
+__version__ = '0.1.2'
+__build__ = '2021032901'
 
 
 import unicodedata
@@ -35,9 +35,28 @@ def convert_accents(string: str) -> str:
     return str(unicodedata.normalize('NFD', string).encode('ascii', 'ignore').decode("utf-8"))
 
 
+def strip_characters(string: str, regex: str = r'[]') -> str:
+    """
+    Remove everything that is not in the list of allowed chars
+    """
+    return re.sub(regex, '', string)
+
+
 def strip_special_characters(string: str, regex: str = r'[^a-zA-Z0-9 \n\._]') -> str:
     """
     Remove special characters except the ones allowed in the regex
     """
-    return re.sub(regex, '', string)
+    return strip_characters(string, regex)
 
+
+def strip_non_alnum_characters(string: str, include_accents: bool = True) -> str:
+    """
+    Return only alphanumeric strings
+    """
+    if include_accents:
+        # Note: \W+ also includes '_' character
+        regex = r'\W+'
+    else:
+        regex = r'[^a-zA-Z0-9]'
+
+    return strip_characters(string, regex)
--- tests/test_string_handling.py
+++ tests/test_string_handling.py
@@ -15,7 +15,7 @@ __intname__ = 'tests.ofunctions.string_handling'
 __author__ = 'Orsiris de Jong'
 __copyright__ = 'Copyright (C) 2020-2021 Orsiris de Jong'
 __licence__ = 'BSD 3 Clause'
-__build__ = '2021020901'
+__build__ = '2021032901'
 
 from ofunctions.string_handling import *
 
@@ -24,7 +24,10 @@ def test_string_handling():
     test_string = 'ABCDxyz1234&é"-è_çà)"ééà()\'"éài^^$¨IÎiï?./%X'
     assert convert_accents(test_string) == 'ABCDxyz1234&e"-e_ca)"eea()\'"eai^^$IIii?./%X', 'Strip accents failed'
     assert strip_special_characters(test_string) == 'ABCDxyz1234_iIi.X', 'Strip special chars failed'
-
+    assert strip_characters(test_string, r'[ABX]') == 'CDxyz1234&é"-è_çà)"ééà()\'"éài^^$¨IÎiï?./%',\
+        'Should return test_string with A, B and X stripped'
+    assert strip_non_alnum_characters(test_string, include_accents=False) == 'ABCDxyz1234iIiX'
+    assert strip_non_alnum_characters(test_string, include_accents=True) == 'ABCDxyz1234éè_çàééàéàiIÎiïX'
 
 if __name__ == '__main__':
     print('Example code for %s, %s' % (__intname__, __build__))
