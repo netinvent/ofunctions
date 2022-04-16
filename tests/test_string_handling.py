5ea40c80eb7f3f12dbae12060ad164d5bcb07ed7
Orsiris de Jong <ozy@netpower.fr>
2022-04-16 18:32:30 +0200

    string_handling: use unicode literals instead of u'str'

--- tests/test_string_handling.py
+++ tests/test_string_handling.py
@@ -10,36 +10,25 @@ Versioning semantics:
     Patch version: Backwards compatible bug fixes
 """
 
+# python 2.7 compat fixes
+from __future__ import unicode_literals
 
 __intname__ = "tests.ofunctions.string_handling"
 __author__ = "Orsiris de Jong"
 __copyright__ = "Copyright (C) 2020-2022 Orsiris de Jong"
 __licence__ = "BSD 3 Clause"
-__build__ = "2022041602"
+__build__ = "2022041603"
 
 
 import sys
-import os
 from ofunctions.string_handling import *
-
-
-def running_on_github_actions():
-    """
-    This is set in github actions workflow with
-          env:
-        RUNNING_ON_GITHUB_ACTIONS: true
-    """
-    return os.environ.get("RUNNING_ON_GITHUB_ACTIONS") == "true"  # bash 'true'
 
 
 def test_string_handling():
     """
     Since we're dealing potentielly with python2, let's have unicode strings for comparaison
     """
-    if sys.version_info[0] < 3:
-        test_string = u'ABCDxyz1234&é"-è_çà)"ééà()\'"éài^^$¨IÎiï?./%X'
-    else:
-        test_string = 'ABCDxyz1234&é"-è_çà)"ééà()\'"éài^^$¨IÎiï?./%X'
+    test_string = 'ABCDxyz1234&é"-è_çà)"ééà()\'"éài^^$¨IÎiï?./%X'
 
     assert (
         convert_accents(test_string) == 'ABCDxyz1234&e"-e_ca)"eea()\'"eai^^$IIii?./%X'
@@ -48,24 +37,16 @@ def test_string_handling():
         strip_special_characters(test_string) == "ABCDxyz1234_iIi.X"
     ), "Strip special chars failed"
 
-    if sys.version_info[0] < 3:
-        strip_characters_result = u'CDxyz1234&é"-è_çà)"ééà()\'"éài^^$¨IÎiï?./%'
-    else:
-        strip_characters_result = 'CDxyz1234&é"-è_çà)"ééà()\'"éài^^$¨IÎiï?./%'
+    strip_characters_result = 'CDxyz1234&é"-è_çà)"ééà()\'"éài^^$¨IÎiï?./%'
     assert (
         strip_characters(test_string, r"[ABX]") == strip_characters_result
     ), "Should return test_string with A, B and X stripped"
     assert (
         strip_non_alnum_characters(test_string, keep_accents=False) == "ABCDxyz1234iIiX"
     )
-    if sys.version_info[0] < 3:
-        strip_non_alnum_characters_result = u"ABCDxyz1234éèçàééàéàiIÎiïX"
-    else:
-        strip_non_alnum_characters_result = "ABCDxyz1234éèçàééàéàiIÎiïX"
 
-    # python 2.7 unicode hell goes on on Github Actions for whatever unholy reasons
-    if not running_on_github_actions():
-        assert (
+    strip_non_alnum_characters_result = "ABCDxyz1234éèçàééàéàiIÎiïX"
+    assert (
             strip_non_alnum_characters(test_string, keep_accents=True)
             == strip_non_alnum_characters_result
         )
