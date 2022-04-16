3cb80b07d2e65bb0f39fb83bf8d6aa1efc02e623
Orsiris de Jong <ozy@netpower.fr>
2022-04-16 12:05:08 +0200

    platform: Update copyright year

--- ofunctions/platform/__init__.py
+++ ofunctions/platform/__init__.py
@@ -15,7 +15,7 @@ Versioning semantics:
 
 __intname__ = "ofunctions.platform"
 __author__ = "Orsiris de Jong"
-__copyright__ = "Copyright (C) 2014-2021 Orsiris de Jong"
+__copyright__ = "Copyright (C) 2014-2022 Orsiris de Jong"
 __description__ = "Very basic platform identification"
 __licence__ = "BSD 3 Clause"
 __version__ = "1.1.0"
