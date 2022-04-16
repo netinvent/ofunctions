0fb63f38568be7034f499c082463b9319703de58
Orsiris de Jong <ozy@netpower.fr>
2022-04-16 11:56:02 +0200

    delayed_keyboardinterrupt: Add tests

--- tests/test_delayed_keyboardinterrupt.py
+++ tests/test_delayed_keyboardinterrupt.py
@@ -10,12 +10,26 @@ Versioning semantics:
     Patch version: Backwards compatible bug fixes
 """
 
-__intname__ = 'tests.ofunctions.delayed_keyboardinterrupt'
-__author__ = 'Orsiris de Jong'
-__copyright__ = 'Copyright (C) 2020-2021 Orsiris de Jong'
-__licence__ = 'BSD 3 Clause'
-__build__ = '2021020901'
+__intname__ = "tests.ofunctions.delayed_keyboardinterrupt"
+__author__ = "Orsiris de Jong"
+__copyright__ = "Copyright (C) 2020-2022 Orsiris de Jong"
+__licence__ = "BSD 3 Clause"
+__build__ = "2022150401"
 
-from ofunctions.checksums import *
 
-# TODO: How to write a CTRL+C test ???
+from time import sleep
+from ofunctions.delayed_keyboardinterrupt import *
+
+# TODO: How to write an automatic CTRL+C test without doing a lot of threading stuff ???
+def test_delayed_keyboard_interrupt():
+    with DelayedKeyboardInterrupt():
+        print(
+            "This is a manual test where you shouldn't be able to use CTRL+C for 2 seconds"
+        )
+        sleep(2)
+        print("done")
+
+
+if __name__ == "__main__":
+    print("Example code for %s, %s" % (__intname__, __build__))
+    test_delayed_keyboard_interrupt()
