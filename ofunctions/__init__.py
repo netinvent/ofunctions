fc6388d36eb27207ca402bc9cf3f09132777404e
Orsiris de Jong <ozy@netpower.fr>
2022-06-30 20:00:12 +0200

    Bump version

--- ofunctions/__init__.py
+++ ofunctions/__init__.py
@@ -18,8 +18,8 @@ __author__ = "Orsiris de Jong"
 __copyright__ = "Copyright (C) 2014-2022 Orsiris de Jong"
 __description__ = "Toolset for files/logging/network/checksumming/process handling"
 __licence__ = "BSD 3 Clause"
-__version__ = "2.3.0"
-__build__ = "2022041601"
+__version__ = "2.4.0"
+__build__ = "2022063001"
 
 
 # Make sure we declare this file as namespace holder
