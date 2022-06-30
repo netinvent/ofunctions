d2b6452e43c6b90af30fbc00e94210c743afc9c9
Orsiris de Jong <ozy@netpower.fr>
2022-06-30 16:14:03 +0200

    threading: Add python 2.7 support

--- ofunctions/threading/__init__.py
+++ ofunctions/threading/__init__.py
@@ -33,42 +33,58 @@ __author__ = "Orsiris de Jong"
 __copyright__ = "Copyright (C) 2019-2022 Orsiris de Jong"
 __description__ = "Threading decorator to run functions as threads"
 __licence__ = "BSD 3 Clause"
-__version__ = "0.1.0"
-__build__ = "2019081101"
-__compat__ = "python3.5+"
+__version__ = "1.0.0"
+__build__ = "2022060301"
+__compat__ = "python2.7+"
 
 
+import sys
 from threading import Thread
-from concurrent.futures import Future
+# Python 2.7 compat fixes
+try:
+    from concurrent.futures import Future
+except ImportError:
+    pass
 from functools import wraps
 
 
-def call_with_future(fn, future, args, kwargs):
-    """
-    Threading a function with return info using Future
-    from https://stackoverflow.com/a/19846691/2635443
+# python 2.7 compat where we will use @threaded
+if sys.version_info[0] < 3:
+    def threaded(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            thread = Thread(target=fn, args=args, kwargs=kwargs)
+            thread.daemon = True
+            thread.start()
+            return thread
+        return wrapper
+else:
+    def call_with_future(fn, future, args, kwargs):
+        """
+        Threading a function with return info using Future
+        from https://stackoverflow.com/a/19846691/2635443
 
-    """
-    try:
-        result = fn(*args, **kwargs)
-        future.set_result(result)
-    except Exception as exc:
-        future.set_exception(exc)
+        """
+        try:
+            result = fn(*args, **kwargs)
+            future.set_result(result)
+        except Exception as exc:
+            future.set_exception(exc)
 
 
-def threaded(fn):
-    """
-    @threaded wrapper in order to thread any function
+    def threaded(fn):
+        """
+        @threaded wrapper in order to thread any function
 
-    @wraps decorator sole purpose is for function.__name__ to be the real function
-    instead of 'wrapper'
+        @wraps decorator sole purpose is for function.__name__ to be the real function
+        instead of 'wrapper'
 
-    """
+        """
 
-    @wraps(fn)
-    def wrapper(*args, **kwargs):
-        future = Future()
-        Thread(target=call_with_future, args=(fn, future, args, kwargs)).start()
-        return future
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            future = Future()
+            Thread(target=call_with_future, args=(fn, future, args, kwargs)).start()
+            return future
 
-    return wrapper
+        return wrapper
