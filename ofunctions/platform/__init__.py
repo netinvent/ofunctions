afcbb7704808f1e59b37403ad6106538da9f1993
Orsiris de Jong <ozy@netpower.fr>
2022-07-05 14:08:43 +0200

    platform: WIP: redhat identification

--- ofunctions/platform/__init__.py
+++ ofunctions/platform/__init__.py
@@ -18,12 +18,13 @@ __author__ = "Orsiris de Jong"
 __copyright__ = "Copyright (C) 2014-2022 Orsiris de Jong"
 __description__ = "Very basic platform identification"
 __licence__ = "BSD 3 Clause"
-__version__ = "1.1.1"
-__build__ = "2022041601"
+__version__ = "1.2.0"
+__build__ = "2022051001"
 __compat__ = "python2.7+"
 
 import os
 import sys
+import re
 
 
 def get_os():
@@ -69,3 +70,49 @@ def is_64bit_python():
     Detect if python is 64 bit but stay OS agnostic
     """
     return sys.maxsize > 2**32
+
+
+def get_distro():
+    # type: (None) -> (str, float, float, float, str)
+    """
+    Detect distribution and version
+    # TODO develop other flavors than RHEL and clones
+    """
+
+    RHEL_path = "/etc/redhat-release"
+    """
+    File content examples:
+    
+    Red Hat Enterprise Server release 7.5 (Maipo)
+    AlmaLinux release 8.5 (Arctic Sphynx)
+    CentOS Linux release 8.4.2105
+    """
+
+    flavor = None
+    version = None
+    major_version = None
+    minor_version = None
+    build = None
+
+    if os.path.isfile(RHEL_path):
+        flavor="RHEL"
+        with open(RHEL_path, 'r') as file_handle:
+            result = re.search(r"(?:(\d+)\.)?(?:(\d+)\.)?(\*|\d+)", file_handle.read(), re.MULTILINE)
+            try:
+                version = result.group(0)
+            except AttributeError:
+                pass
+            try:
+                major_version = result.group(1)
+            except IndexError:
+                pass
+            try:
+                minor_version = result.group(2)
+            except IndexError:
+                pass
+            try:
+                build = result.group(3)
+            except IndexError:
+                pass
+
+    return flavor, version, major_version, minor_version, build
