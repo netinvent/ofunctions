ac292848528ebbd39f37c8b21521b7ef40178ff9
Orsiris de Jong <ozy@netpower.fr>
2024-01-05 14:39:18 +0100

    platform: Remove get_distro, add simple get_os_identifier()

--- ofunctions/platform/__init__.py
+++ ofunctions/platform/__init__.py
@@ -15,16 +15,15 @@ Versioning semantics:
 
 __intname__ = "ofunctions.platform"
 __author__ = "Orsiris de Jong"
-__copyright__ = "Copyright (C) 2014-2023 Orsiris de Jong"
+__copyright__ = "Copyright (C) 2014-2024 Orsiris de Jong"
 __description__ = "Very basic platform identification"
 __licence__ = "BSD 3 Clause"
-__version__ = "1.4.1"
-__build__ = "2023033001"
+__version__ = "1.5.0"
+__build__ = "2024010301"
 __compat__ = "python2.7+"
 
 import os
 import sys
-import re
 import platform
 
 
@@ -57,7 +56,7 @@ def os_arch():
     # armv71, aarch64, x86_64
     # windows:
     # AMD64x
-    machine = platform.machine()
+    machine = platform.machine() 
     if machine == "AMD64":
         return "x64"
     machine = machine.lower()
@@ -66,7 +65,7 @@ def os_arch():
     elif "aarch64" in machine or "armv8" in machine:
         return "arm64"
     # 32 bit arm
-    elif "arm" in machine:
+    elif "arm" in machine: 
         return "arm"
     elif "i386" in machine or "i686" in machine:
         return "x86"
@@ -109,49 +108,13 @@ def is_64bit_python():
     return sys.maxsize > 2**32
 
 
-def get_distro():
-    # type: () -> (str, float, float, float, str)
+def get_os_identifier():
+    # type: () -> dict
     """
-    Detect distribution and version
-    # TODO develop other flavors than RHEL and clones
+    Returns a dict of os info
     """
-
-    RHEL_path = "/etc/redhat-release"
-    """
-    File content examples:
-
-    Red Hat Enterprise Server release 7.5 (Maipo)
-    AlmaLinux release 8.5 (Arctic Sphynx)
-    CentOS Linux release 8.4.2105
-    """
-
-    flavor = None
-    version = None
-    major_version = None
-    minor_version = None
-    build = None
-
-    if os.path.isfile(RHEL_path):
-        flavor = "RHEL"
-        with open(RHEL_path, "r") as file_handle:
-            result = re.search(
-                r"(?:(\d+)\.)?(?:(\d+)\.)?(\*|\d+)", file_handle.read(), re.MULTILINE
-            )
-            try:
-                version = result.group(0)
-            except AttributeError:
-                pass
-            try:
-                major_version = result.group(1)
-            except IndexError:
-                pass
-            try:
-                minor_version = result.group(2)
-            except IndexError:
-                pass
-            try:
-                build = result.group(3)
-            except IndexError:
-                pass
-
-    return flavor, version, major_version, minor_version, build
+    return {
+        "type": get_os(),
+        "platform": platform.platform(),
+        "version": platform.version(),
+    }
